f6609f805dd68da3edcdf2e703620e781a0b611d
MatthieuGenovese <matthieu.genovese@gmail.com>
2018-10-08 20:07:53 +0200

    Concept Spells / rota

--- src/model/classes/Mage.java
+++ src/model/classes/Mage.java
@@ -1,4 +1,20 @@
 package model.classes;
 
+import model.gear.Equipment;
+
+import java.util.ArrayList;
+
 public class Mage implements Classe {
+
+    private String spec;
+
+    private ArrayList<Equipment> gear;
+
+    private Rotation rotation;
+
+    public Mage (String spec, Rotation rotation, ArrayList<Equipment> equipment){
+        this.spec = spec;
+        this.rotation = rotation;
+        this.gear = equipment;
+    }
 }
--- src/model/classes/Spell.java
+++ src/model/classes/Spell.java
@@ -1,4 +1,26 @@
 package model.classes;
 
 public class Spell {
+    private int id;
+    private double scalling;
+    private int spellpower;
+    private double damage;
+    private int cooldown;
+    private int casttime;
+
+
+    public Spell(int id, int scalling, int casttime, int cooldown){
+        this.id=id;
+        this.scalling=scalling;
+        this.casttime = casttime;
+        this.cooldown = cooldown;
+    }
+
+    public void setSpellpower(int spellpower){
+        this.spellpower=spellpower;
+    }
+
+    public void computeDamage(){
+        this.damage = spellpower * scalling;
+    }
 }
